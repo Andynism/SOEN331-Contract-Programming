7ef262b9961b9cd1dce7b8aacd3cb265e6cdb5fd
Stromation <leamsem719@hotmail.ca>
2019-03-11 15:09:16 -0400

    Added stuff from last semester

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,3 +1,103 @@
 public class PriorityQueue {
+    private int lastElementIndex, capacity;
+  	private Element[] data;	//Array that contains the data
 
+  	//Constructor
+  	public PriorityQueue() {
+  		this(100);
+  	}
+
+  	public PriorityQueue(int size) {
+  		this.capacity = size*2;
+  		this.data = new Element[this.capacity];
+  		lastElementIndex = 0;
+  	}
+
+  	//Insert job in array
+  	public void insert(Element value) {
+  		data[++lastElementIndex] = value;
+  	}
+
+  	//Remove the smallest element
+  	public Element removeMin() {
+  		if(isEmpty()){
+  			System.out.println("The Priority Queue is empty.");
+  			return null;
+  		}
+  		int positionToRemove = 0;
+  		while(positionToRemove <= lastElementIndex)  {
+  			if(data[positionToRemove] != null)
+  				break;
+  			positionToRemove++;
+  		}
+  		int i = 0;
+  		while(i < lastElementIndex) {
+  			if(data[positionToRemove] != null && data[i] != null) {
+  				if(data[i].getFinalPriority() < data[positionToRemove].getFinalPriority())
+  					positionToRemove = i;
+  			}
+  			i++;
+
+  		}
+
+  		Element min = data[positionToRemove];
+  		data[positionToRemove] = null;
+  		return min;
+  	}
+
+  	//Return the smallest element
+  	public Element getMin() {
+  		if(isEmpty()){
+  			System.out.println("The Priority Queue is empty.");
+  			return null;
+  		}
+  		int positionToRemove = 0;
+  		while(positionToRemove <= lastElementIndex)  {
+  			if(data[positionToRemove] != null)
+  				break;
+  			positionToRemove++;
+  		}
+  		int i = 0;
+  		while(i < lastElementIndex-1) {
+  			if(data[positionToRemove] != null && data[i] != null) {
+  				if(data[i].getFinalPriority() < data[positionToRemove].getFinalPriority())
+  					positionToRemove = i;
+  			}
+  			i++;
+
+  		}
+
+
+  		return data[positionToRemove];
+  	}
+
+  	public boolean isEmpty(){
+  		Element a = data[0];
+  		for(int i=0;i<data.length;i++) {
+  			if(data[i] != null)
+  				a = data[i];
+  		}
+  		if(a != null) {
+  			return false;
+  		}
+
+  		return true;
+  	}
+
+  	public boolean isFull(){
+  		return lastElementIndex == capacity - 1;
+  	}
+
+  	public String toString() {
+  		String ans = "";
+  		for(int i = 1; i < lastElementIndex; i++) {
+  			ans += data[i].toString();
+  			ans +="\n";
+  		}
+  		return ans;
+  	}
+
+  	public int getSize() {
+  		return data.length;
+  	}
 }
--- PriorityQueueDriver.java
+++ PriorityQueueDriver.java
@@ -1,3 +1,41 @@
 public class PriorityQueueDriver {
+  public static void main(String[] args){
+    int maxNumberOfElement = 500;
+    Element[] elementsInputArray = new Element[maxNumberOfElement];
+    Element executing;
+    String elementName;
+    int elementPriority;
+    int counterOfElementDone = 0;
 
+    //Setting the jobsInputArray for all the testing
+    for(int i=0;i<maxNumberOfElement;i++) {
+    		elementName = "Element_" + (i+1);
+    		elementPriority = (int) (Math.random()*100+1);
+    		elementsInputArray[i] = new Element(elementName,elementPriority);
+    }
+
+
+    PriorityQueue pq1 = new PriorityQueue(maxNumberOfElement);
+
+    //Insert elements to ADT
+		for(int i=0;i<maxNumberOfElement;i++) {
+			pq1.insert(elementsInputArray[i]);
+		}
+
+    //Loop to do all the jobs in the ADT
+		long start = System.currentTimeMillis();
+		while(!pq1.isEmpty()) {
+			executing = pq1.getMin();//Get most important job
+			if(executing != null) {
+				System.out.println("Now executing " + executing.toString());
+
+				pq1.removeMin();//Remove the job
+				counterOfElementDone++;
+			}
+
+		}
+		long end = System.currentTimeMillis();
+
+    System.out.print("Number of elements done:" + counterOfElementDone);
+  }
 }
